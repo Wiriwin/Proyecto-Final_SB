aef38d1d7ec641408001ab8aad9b1e72d6ef19e5
Mishelle963 <katherinealarconc924yep.exp@gmail.com>
2025-08-20 09:34:16 -0600

    god

--- CN1.php
+++ CN1.php
@@ -22,16 +22,16 @@
     <h1 class="titulo">Ciencias Naturales</h1>
 
     <div class="block1">
-        <div class="tema">Partes externas de cuerpo humano</div>
-        <div class="tema">Partes internas de cuerpo humano</div>
-        <div class="tema">Factores bióticos y abióticos</div>
-        <div class="tema">Sistema Respiratorio</div>
+        <button class="tema">Partes externas de cuerpo humano</button>
+        <button class="tema">Partes internas de cuerpo humano</button>
+        <button class="tema">Factores bióticos y abióticos</button>
+        <button class="tema">Sistema Respiratorio</button>
     </div>
     <div class="block1">
-        <div class="tema">Ecosistemas</div>
-        <div class="tema">Prácticas de hábitos de higiene corporal</div>
-        <div class="tema">Partes de la planta</div>
-        <div class="tema">Tema</div>
+        <button class="tema">Ecosistemas</button>
+        <button class="tema">Prácticas de hábitos de higiene corporal</button>
+        <button class="tema">Partes de la planta</button>
+        <button class="tema">Tema</button>
     </div>
     <div class="block1">
         <div></div>

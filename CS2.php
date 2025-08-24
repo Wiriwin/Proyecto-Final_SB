382764ae578cd2d3bddd72a2d6ca941c744cbff9
Bricty <britneymorales347@gmail.com>
2025-08-24 17:51:11 -0600

    probando nenes, no me deja nada

--- CS2.php
+++ CS2.php
@@ -21,7 +21,7 @@
     </header>
 
     <h1 class="titulo">Ciencias Sociales</h1>
-    <h1 class="subtitulo">Tercer y Cuarto grado</h1>
+    <h1 class="subtitulo">ya no doy</h1>
 
     <div class="block1">
         <a href= "pdf_mate_1\primero_segundo\Asociación del Conjunto vacío con el Numeral Cero.pdf" class="tema" class="tema">Conquista de América</a>

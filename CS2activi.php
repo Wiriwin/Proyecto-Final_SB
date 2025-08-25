fc0843fcd5fbc3e2dfedc00a13cdafb5d4d48e08
Joleoos <julioescobar535g@gmail.com>
2025-08-25 11:04:44 -0600

    ass

--- CS2activi.php
+++ CS2activi.php
@@ -29,16 +29,10 @@
         <a href= "pdf_sociales_2\Tercero_cuartoS\historia nacional y cultural actividad.pdf" class="tema" class="tema">Historia Nacional y Cultural</a>
         <a href= "pdf_sociales_2\Tercero_cuartoS\actividades de transportes.pdf" class="tema" class="tema">Medios de Transporte</a>
     </div>
-    <div class="block1">
-        <a href= "pdf_mate_1\primero_segundo\Asociación del Conjunto vacío con el Numeral Cero.pdf" class="tema" class="tema">Actividades económicas de Guatemala</a>
-        <a href= "pdf_mate_1\primero_segundo\Asociación del Conjunto vacío con el Numeral Cero.pdf" class="tema" class="tema">Convivencia democrática y derechos humanos</a>
-        <a href= "pdf_mate_1\primero_segundo\Asociación del Conjunto vacío con el Numeral Cero.pdf" class="tema" class="tema">Funciones del estado y organizaciones</a>
-        <a href= "pdf_mate_1\primero_segundo\Asociación del Conjunto vacío con el Numeral Cero.pdf" class="tema" class="tema">Participación ciudadana y equidad</a>
-    </div>
+   
     <div class="block1">
         <div></div>
         <a href= "pdf_sociales_2\Tercero_cuartoS\Actividad de Municipios y departamentos.pdf" class="tema" class="tema">Municipios y departamentos </a>
-        <a href= "pdf_mate_1\primero_segundo\Asociación del Conjunto vacío con el Numeral Cero.pdf" class="tema" class="tema">Resolución solidaria de conflictos y desigualdades</a>
         <div></div>
     </div>
 

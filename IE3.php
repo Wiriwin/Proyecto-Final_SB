563ab5f75c15eff39eb41e08c70668ea855149a7
Mishelle963 <katherinealarconc924yep.exp@gmail.com>
2025-08-25 08:43:51 -0600

    hello

--- IE3.php
+++ IE3.php
@@ -24,21 +24,21 @@
       <h1 class="subtitulo"> Quinto y Sexto Grado</h1>
 
     <div class="block1">
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Campos semánticos.pdf" class="tema" class="tema">Campos semánticos</a>
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Conjunciones.pdf" class="tema" class="tema">Conjunciones</a>
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Ideas principales.pdf" class="tema" class="tema">Ideas principales</a>
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Ideas secundarias.pdf" class="tema" class="tema">Ideas secundarias</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Campos semánticos.pdf" class="tema">Campos semánticos</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Conjunciones.pdf" class="tema">Conjunciones</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Ideas principales.pdf" class="tema">Ideas principales</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Ideas secundarias.pdf" class="tema">Ideas secundarias</a>
     </div>
     <div class="block1">
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Palabras agudas, graves y esdrújulas.pdf" class="tema" class="tema">Palabras agudas,graves y esdrújulas</a>
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Palabras homófonas y homógrafas.pdf" class="tema" class="tema">Palabras homófonas y homógrafas</a>
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Prefijos.pdf" class="tema" class="tema">Prefijos</a>
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Sílabas átonas.pdf" class="tema" class="tema">Sílabas átonas</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Palabras agudas, graves y esdrújulas.pdf" class="tema" >Palabras agudas,graves y esdrújulas</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Palabras homófonas y homógrafas.pdf" class="tema" >Palabras homófonas y homógrafas</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Prefijos.pdf" class="tema" >Prefijos</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Sílabas átonas.pdf" class="tema" >Sílabas átonas</a>
     </div>
     <div class="block1">
         <div></div>
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Sílabas tónicas.pdf" class="tema" class="tema">Sílabas tónicas</a>
-        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Sufijos.pdf" class="tema" class="tema">Sufijos</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Sílabas tónicas.pdf" class="tema" >Sílabas tónicas</a>
+        <a href= "pdf-comunicacion-1\comunicacion_1\quinto_sexto\Sufijos.pdf" class="tema" >Sufijos</a>
         <div></div>
     </div>
 

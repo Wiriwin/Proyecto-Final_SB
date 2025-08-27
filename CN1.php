26327141c8adbef4814dad70225375d08b881f7c
Joleoos <julioescobar535g@gmail.com>
2025-08-27 11:21:11 -0600

    ass

--- CN1.php
+++ CN1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/universal-diseño.css">
-    <link rel="Stylesheet" href="css\CN-diseñitos.css">
+    <link rel="Stylesheet" href="css\CN-style.css">
     <title>Lecciones y Actividades</title>
 </head>
 <body>
